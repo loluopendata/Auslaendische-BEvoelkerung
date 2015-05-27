685e4dcc130511ddf402392bac55be21f67a29e3
Lukas Heppler <Lukas@Lukass-MacBook-Pro-2.local>
2015-05-27 23:31:51 +0200

    moved scripts to header & changed descriptions

--- map.js
+++ map.js
@@ -1,5 +1,7 @@
  
-var defaultScale;         
+var defaultScale;
+
+var defaultDataSet = 'Gemeinden_2013.csv';
 
 var colours2 = {
     Asien: ['rgb(252,251,253)','rgb(239,237,245)','rgb(218,218,235)','rgb(188,189,220)','rgb(158,154,200)','rgb(128,125,186)','rgb(106,81,163)','rgb(84,39,143)','rgb(63,0,125)'], Amerika:['rgb(255,245,240)','rgb(254,224,210)','rgb(252,187,161)','rgb(252,146,114)','rgb(251,106,74)','rgb(239,59,44)','rgb(203,24,29)','rgb(165,15,21)','rgb(103,0,13)'], Europa:['rgb(247,252,253)','rgb(229,245,249)','rgb(204,236,230)','rgb(153,216,201)','rgb(102,194,164)','rgb(65,174,118)','rgb(35,139,69)','rgb(0,109,44)','rgb(0,68,27)'], Ozeanien:['rgb(255,255,255)','rgb(240,240,240)','rgb(217,217,217)','rgb(189,189,189)','rgb(150,150,150)','rgb(115,115,115)','rgb(82,82,82)','rgb(37,37,37)','rgb(0,0,0)'], Afrika: ['rgb(255,245,235)','rgb(254,230,206)','rgb(253,208,162)','rgb(253,174,107)','rgb(253,141,60)','rgb(241,105,19)','rgb(217,72,1)','rgb(166,54,3)','rgb(127,39,4)'], Andere:['rgb(255,255,255)','rgb(240,240,240)','rgb(217,217,217)','rgb(189,189,189)','rgb(150,150,150)','rgb(115,115,115)','rgb(82,82,82)','rgb(37,37,37)','rgb(0,0,0)']};
@@ -33,23 +35,50 @@ function drawLegend(scale){
 }
 
 
-var dataset = []
+var dataset = [];
 var rows;
 
 var width = 860, 
     height = 500, 
     colors = d3.scale.category20();
     
-var svg = d3.select("#map").append("svg")
+var svg = d3.select("#map").append("g").append("svg")
     .attr("width", width)
     .attr("height", height);
     
 var div = d3.select("body").append("div")
     .attr("class","tooltip")
     .style("opacity",0);
+
+function showTooltip(){
+    div.transition()
+        .duration(150)
+        .style("opacity",.9);
+
+}
+
+function hideTooltip(){
+    div.transition()
+        .duration(150)
+        .style("opacity",0);
+
+}
    
 
-    
+ var zoom = d3.behavior.zoom()
+      .scaleExtent([1, 10])
+      .on('zoom', zoomMap);
+d3.select("#map").select("g").call(zoom);
+
+function zoomMap() {
+
+  // Zoom keeping stroke width proportional inspired by Ivo Vonlanthen
+  d3.select('#map').select('svg').selectAll(".municipality-boundaries").attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")").style("stroke-width", .5 / d3.event.scale + "px");
+    hideTooltip();
+
+}
+
+
 function loadDataSet(path){
     //load CSV File
     d3.csv(path, function(row) {
@@ -70,7 +99,7 @@ function loadDataSet(path){
 
 }
 
-loadDataSet("Gemeinden_2013.csv");
+loadDataSet(defaultDataSet);
 
 
 
@@ -115,20 +144,19 @@ function processRows() {
     });
             
 
-	var max = d3.max(rows,function(d){return (1-(d['Schweiz']/d['Total']))});
+	var max = d3.max(rows,function(d){return (1-(d['Schweiz']/d['Total']));});
         
     defaultScale = d3.scale.quantize()
         .domain(d3.range(0,max,max/(colours2['Europa'].length-1)))
         .range(colours2['Europa']);
         
     drawLegend(defaultScale);
-   
     svg.selectAll(".subunit")
         .data(gemeinden)
         .enter()
         .append("path")
         .attr("class", "municipality-boundaries")
-        .attr("id", function(d){return d.id})
+        .attr("id", function(d){return d.id;})
         .property("data", function(d){return d.properties.data;}) //doppelt drin?
         .attr("name", function(d){return d.properties.name;})
         .attr("fill", function (d) {
@@ -153,9 +181,7 @@ function processRows() {
            }
         })
         .on("mouseout",function(e,f){
-            div.transition()
-                .duration(150)
-                .style("opacity",0);
+            hideTooltip();
         
            d3.select(this).style('stroke', '#000');
            
@@ -182,9 +208,7 @@ function processRows() {
                     percent = Math.round((((tmp[filter])/ tmp.Total)*100) * 100) / 100, '%)';
                 }
                
-                div.transition()
-                    .duration(150)
-                    .style("opacity",.9);
+                showTooltip();
                 
                 div
                     .html(['<strong>', e.properties.name , '</strong><br/>',
@@ -209,7 +233,7 @@ function prepareJSON(e){
         if (world.hasOwnProperty(key)) {
             var tmpObject = {name: key, children:[]};
             for(var i=0;i<world[key].length;i++){
-                var x =  e[world[key][i]]
+                var x =  e[world[key][i]];
                 if(x>0){
                     tmpObject.children.push({name: world[key][i], size: x});
                 }
@@ -270,7 +294,7 @@ function updateMap(bfs,name,link){
         d3.select('#piechart').attr('filter',name);
         
         var max = d3.max(d3.select('#map').select('svg').selectAll('path')[0],function(d){if(d.id>9000){
-        return 0}else{var tmp = d.__data__.properties.data; return (tmp[name]/tmp['Total']);}});
+        return 0;}else{var tmp = d.__data__.properties.data; return (tmp[name]/tmp['Total']);}});
         
         var newScale = d3.scale.quantize()
             .domain(d3.range(0,max,max/(colours2[name].length-1)))
@@ -374,15 +398,13 @@ function drawFlare(data){
         .style("stroke", "#fff")
         .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
         .style("fill-rule", "evenodd")
-        .attr("name", function(d){return d.name})
+        .attr("name", function(d){return d.name;})
         .each(stash)
         .on("mouseover",function(e,f){
                
                if(e.name!="root"){
                    
-                   div.transition()
-                    .duration(150)
-                    .style("opacity",.9);
+                   showTooltip();
                    
                     div .html([
                     '<strong>', e.name , '</strong><br/>',
@@ -408,21 +430,9 @@ function drawFlare(data){
         })
     
         .on("mouseout",function(e,f){
-                 div.transition()
-                    .duration(150)
-                    .style("opacity",0);
+                 hideTooltip();
              });
     
-/*function change(method) {
-    var value = method === "count"
-        ? function() { return 1; }
-        : function(d) { return d.size; };
-    path //= svg3.datum(value).selectAll("path")
-        .data(partition.value(value).nodes)
-      .transition()
-        .duration(1000)
-        .attrTween("d", arcTweenData);
-  }*/
 
   
     //prepare Legend
@@ -453,7 +463,7 @@ function drawFlare(data){
     
     legend.append('rect')
         .attr('width', legendRectSize)
-        .style('display',function(d,i){return (i==0)?'none':'initial'})
+        .style('display',function(d,i){return (i==0)?'none':'initial';})
         .attr('height', legendRectSize)
         .style('fill', color)
         .style('stroke', color);
@@ -463,7 +473,7 @@ function drawFlare(data){
         .attr('y', legendRectSize - legendSpacing)
         .text(function(d) { return d; }).style("fill","#00")
         .style('font-family','sans-serif').style("display",function(d){
-            return (d=='root')? 'none':'initial'
+            return (d=='root')? 'none':'initial';
         });
 
 
@@ -549,4 +559,8 @@ function overlay() {
 
 d3.select("#info").on("click", function(d){
     overlay();
+});
+d3.select("#refresh").on("click", function(d){
+    d3.select("#year").property('selectedIndex',0);
+    loadDataSet(defaultDataSet);
 });
